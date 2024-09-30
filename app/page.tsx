c4c0361b4a355f78556a37b07919eb44308c51d7
Bryan Bergenholtz <116790009+YanRho@users.noreply.github.com>
2024-09-29 18:19:17 -0700

    Folder organization

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello World!</p>
-      <Button>Click Here</Button>
+      <p className="text-3xl font-bold text-indigo-500">
+        Hello Discord Clone
+      </p>
+      <Button>
+        Click Here
+      </Button>
     </div>
   );
 }
