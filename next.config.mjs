bc66a161e793ac80a890d13f4eb4af8bc850d1a1
Bryan Bergenholtz <116790009+YanRho@users.noreply.github.com>
2024-10-11 19:22:02 -0700

    utfs.io removed

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'uploadthing.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-      },
+      }
     ],
   },
 };
